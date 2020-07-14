99404e1ff0d15336f963c2a15b6fb9ba05c33877
Fredie <madrigal891@gmail.com>
2020-07-14 13:31:50 +0530

    Initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,19 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'images/1.jpg': 'center',
+							'images/2.jpg': 'center',
+							'images/3.jpg': 'center',
+							'images/4.jpg': 'center',
+							'images/5.jpg': 'center',
+							'images/6.jpg': 'center',
+							'images/7.jpg': 'center',
+							'images/8.jpg': 'center',
+							'images/9.jpg': 'center',
+							'images/10.jpg': 'center',
+							'images/11.jpg': 'center',
+							'images/0.jpg': 'center'
+
 						},
 
 					// Delay.
